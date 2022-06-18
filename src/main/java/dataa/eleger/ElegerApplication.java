a06a80440f92be59bd27931126baefe8a2c1c771
theCoyote <flaviolunaferreira@gmail.com>
2022-06-18 11:24:57 -0300

    iniciando aplicação

---
+++ src/main/java/dataa/eleger/ElegerApplication.java
@@ -0,0 +1,13 @@
+package dataa.eleger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dataa/eleger/ElegerApplicationTests.java
@@ -0,0 +1,13 @@
+package dataa.eleger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
