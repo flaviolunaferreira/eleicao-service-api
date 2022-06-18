8073a61a81ee01cb663d218b9ca94f3fb5b0ca74
theCoyote <flaviolunaferreira@gmail.com>
2022-06-18 16:13:56 -0300

    EleicaoRepositorio

--- src/main/java/dataa/eleger/repositorios/EleicaoRepositorio.java
+++ src/main/java/dataa/eleger/repositorios/EleicaoRepositorio.java
@@ -3,5 +3,5 @@ package dataa.eleger.repositorios;
 import dataa.eleger.entidades.EleicaoEntidade;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EleicaoRepositorio extends PagingAndSortingRepository<EleicaoEntidade, Integer> {
+public interface EleicaoRepositorio extends PagingAndSortingRepository<EleicaoEntidade, Long> {
 }
